08f5a9a29ccddda1321726a1bf54294986c60438
kit <bkprasb@gmail.com>
2015-01-27 12:03:47 -0600

    initial commit

---
+++ GitHubProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Git Hub This is my first Project \n . I am planning to implement remote rpository");
+
+            Console.ReadLine();
+        }
+    }
+}
